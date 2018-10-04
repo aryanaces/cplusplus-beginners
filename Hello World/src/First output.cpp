d23a76fa4a8427efa689e8a522853961c1300d91
aryanaces <43854758+aryanaces@users.noreply.github.com>
2018-10-04 22:37:37 +0530

    updated the entire code
    Changed the code to an output text which I believe will make the user understand the syntax of outputting better . Along with a comment to explain the use cout<<"";

---
+++ Hello World/src/First output.cpp
@@ -0,0 +1,12 @@
+// First Output
+// A basic program to familiarize with the basic syntax of C++ programming
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "The text that you need to display as an output (on the screen)" << endl; // cout<<""; is used for outputing a string/text.
+
+	return 0;// the ' ; ' marks the end of the line / line of code.
+}
